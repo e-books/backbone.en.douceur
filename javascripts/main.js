db980575624d0e2239d3ac9278148e76f86bb4b5
k33g_org <ph.charriere@gmail.com>
2012-11-16 22:20:37 +0100

    ...

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,50 +2,15 @@
 
 $(function (){
 	
-	var k33g = new Gh3.User("k33g")
-	,   k33gBlog = new Gh3.Repository("backbone.en.douceur", k33g);
+	var converter = new Showdown.converter();
+	$.get("README.md", function(page) { 
+		$("#main_content").html(converter.makeHtml(page));
+		$.get("00-PREAMBULE.md", function(page) { 
+			$("#preambule").html(converter.makeHtml(page));
+		} );
+	} );
 
-	k33gBlog.fetch(function (err, res) {
-	    if(err) { throw "outch ..." }
-
-	    var mdConverter = new Showdown.converter();
-
-	    k33gBlog.fetchBranches(function (err, res) {
-	        if(err) { throw "outch ..." }
-
-	        var master = k33gBlog.getBranchByName("master");
-
-	        master.fetchContents(function (err, res) {
-	            if(err) { throw "outch ..." }
-
-
-			    window.readme = master.getFileByName("README.md");
-				window.preambule = master.getFileByName("00-PREAMBULE.md");
-
-			    readme.fetchContent(function (err, res) {
-			        if(err) { throw "outch ..." }
-
-			        var content = mdConverter.makeHtml(readme.getRawContent());
-					$("#main_content").html(content);
-
-				    preambule.fetchContent(function (err, res) {
-				        if(err) { throw "outch ..." }
-
-				        var content = mdConverter.makeHtml(preambule.getRawContent());
-						$("#preambule").html(content);
-
-				    });
-
-
-			    });
-
-	        });
-
-	    })
-	});
-
-
-}); //End of $(function (){...}	
+}); 
 
 
 
