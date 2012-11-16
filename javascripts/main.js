9cb65598e2c6da3a61153139d3133fe5d36091d5
k33g_org <ph.charriere@gmail.com>
2012-11-16 21:53:26 +0100

    www

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,36 +2,53 @@
 
 $(function (){
 	
-	var mdConverter = new Showdown.converter();
-	
-	$.ajax({
-		contentType : 'application/text',
-		url : 'https://raw.github.com/k33g/backbone.en.douceur/master/README.md',
-		success: function(data) {
-			//console.log(data);
-			var content = mdConverter.makeHtml(data);
-			$("#main_content").html(content);
+	var k33g = new Gh3.User("k33g")
+	,   k33gBlog = new Gh3.Repository("backbone.en.douceur", k33g);
 
-			$.ajax({
-				contentType : 'application/text',
-				url : 'https://raw.github.com/k33g/backbone.en.douceur/master/00-PREAMBULE.md',
-				success: function(data) {
-					//console.log(data);
-					var content = mdConverter.makeHtml(data);
-					$("#preambule").html(content);
-				},
-				error : function(err) {
-					throw err;
-				}
-			});
+	k33gBlog.fetch(function (err, res) {
+	    if(err) { throw "outch ..." }
 
-		},
-		error : function(err) {
-			throw err;
-		}
+	    var mdConverter = new Showdown.converter();
+
+	    k33gBlog.fetchBranches(function (err, res) {
+	        if(err) { throw "outch ..." }
+
+	        var master = k33gBlog.getBranchByName("master");
+
+	        master.fetchContents(function (err, res) {
+	            if(err) { throw "outch ..." }
+
+
+			    window.readme = master.getFileByName("README.md");
+				window.preambule = master.getFileByName("00-PREAMBULE.md");
+
+			    readme.fetchContent(function (err, res) {
+			        if(err) { throw "outch ..." }
+
+			        var content = mdConverter.makeHtml(readme.getRawContent());
+					$("#main_content").html(content);
+
+				    preambule.fetchContent(function (err, res) {
+				        if(err) { throw "outch ..." }
+
+				        var content = mdConverter.makeHtml(preambule.getRawContent());
+						$("#preambule").html(content);
+
+				    });
+
+
+			    });
+
+	        });
+
+	    })
 	});
 
 
 }); //End of $(function (){...}	
+
+
+
+
 
 
