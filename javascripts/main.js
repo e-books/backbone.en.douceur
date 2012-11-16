0e3196c8135de814c495e1495a320d9b22908bfb
k33g_org <ph.charriere@gmail.com>
2012-11-16 21:19:44 +0100

    www

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,37 @@
-console.log('This would be the main JS file.');
+
+
+$(function (){
+	
+	var mdConverter = new Showdown.converter();
+	
+	$.ajax({
+		contentType : 'application/text',
+		url : 'https://raw.github.com/k33g/backbone.en.douceur/master/README.md',
+		success: function(data) {
+			//console.log(data);
+			var content = mdConverter.makeHtml(data);
+			$("#main_content").html(content);
+
+			$.ajax({
+				contentType : 'application/text',
+				url : 'https://raw.github.com/k33g/backbone.en.douceur/master/00-PREAMBULE.md',
+				success: function(data) {
+					//console.log(data);
+					var content = mdConverter.makeHtml(data);
+					$("#preambule").html(content);
+				},
+				error : function(err) {
+					throw err;
+				}
+			});
+
+		},
+		error : function(err) {
+			throw err;
+		}
+	});
+
+
+}); //End of $(function (){...}	
+
+
