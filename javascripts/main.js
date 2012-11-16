8561eb39470debe21ce15374acd7f0459dec6f10
k33g_org <ph.charriere@gmail.com>
2012-11-16 23:24:58 +0100

    ...

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 
-
+/*
 $(function (){
 	
 	var converter = new Showdown.converter();
@@ -11,9 +11,45 @@ $(function (){
 	} );
 
 }); 
+*/
+
+$(function (){
+	var k33g = new Gh3.User("k33g")
+	,   k33gBlog = new Gh3.Repository("backbone.en.douceur", k33g)
+	,   converter = new Showdown.converter();
+
+	k33gBlog.fetch(function (err, res) {
+	    if(err) { throw "outch ..." }
+
+	    k33gBlog.fetchBranches(function (err, res) {
+	        if(err) { throw "outch ..." }
+
+	        var master = k33gBlog.getBranchByName("master");
+
+	        master.fetchContents(function (err, res) {
+	            if(err) { throw "outch ..." }
 
 
+			    var readme = master.getFileByName("README.md");
+				var preambule = master.getFileByName("00-PREAMBULE.md");
+
+			    readme.fetchContent(function (err, res) {
+			        
+			        if(err) { throw "outch ..." }
+			        $("#main_content").html(converter.makeHtml(readme.getRawContent()));
+
+				    preambule.fetchContent(function (err, res) {
+				        if(err) { throw "outch ..." }
+				        $("#preambule").html(converter.makeHtml(preambule.getRawContent()));
+				    });
+
+			    });
 
 
+	        });
+
+	    })
+	});
+}); 
 
 
